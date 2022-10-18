676e12adbd871cafdac22a0bbabda237653de721
marceesty <marceesty@gmail.com>
2022-10-18 12:13:21 +0100

    added migrations

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,24 +1,145 @@
 package main
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"os"
 
-	"github.com/go-chi/chi/v5"
-	"github.com/go-chi/chi/v5/middleware"
-	"github.com/marceesty/bookings/pkg/config"
-	"github.com/marceesty/bookings/pkg/handlers"
+	"github.com/alexedwards/scs/v2"
 )
 
-func routes(app *config.AppConfig) http.Handler {
+const portNumber = ":8080"
 
-	mux := chi.NewRouter()
+var app config.AppConfig
+var session *scs.SessionManager
+var infoLog *log.Logger
+var errorLog *log.Logger
 
-	mux.Use(middleware.Recoverer)
-	mux.Use(NoSurf)
-	mux.Use(SessionLoad)
+// main is the main application function
+func main() {
+	db, err := run()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.SQL.Close()
 
-	mux.Get("/", handlers.Repo.Home)
-	mux.Get("/about", handlers.Repo.About)
+	defer close(app.MailChan)
 
-	return mux
+	fmt.Println("Staring mail listener...")
+	listenForMail()
+
+	fmt.Println(fmt.Sprintf("Starting application on port %s", portNumber))
+
+	srv := &http.Server{
+		Addr:    portNumber,
+		Handler: routes(&app),
+	}
+
+	err = srv.ListenAndServe()
+	log.Fatal(err)
 }
+
+// func run() (*driver.DB, error) {
+	// // what am I going to put in the session
+	// gob.Register(models.Reservation{})
+	// gob.Register(models.User{})
+	// gob.Register(models.Room{})
+	// gob.Register(models.Restriction{})
+	// gob.Register(map[string]int{})
+
+	// // read flags
+	// inProduction := flag.Bool("production", true, "Application is in production")
+	// useCache := flag.Bool("cache", true, "Use template cache")
+	// dbHost := flag.String("dbhost", "localhost", "Database host")
+	// dbName := flag.String("dbname", "", "Database name")
+	// dbUser := flag.String("dbuser", "", "Database user")
+	// dbPass := flag.String("dbpass", "", "Database password")
+	// dbPort := flag.String("dbport", "5432", "Database port")
+	// dbSSL := flag.String("dbssl", "disable", "Database ssl settings (disable, prefer, require)")
+
+	// flag.Parse()
+
+	// if *dbName == "" || *dbUser == "" {
+	// 	fmt.Println("Missing required flags")
+	// 	os.Exit(1)
+	// }
+
+	mailChan := make(chan models.MailData)
+	app.MailChan = mailChan
+
+	// change this to true when in production
+	app.InProduction = *inProduction
+	app.UseCache = *useCache
+
+	infoLog = log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	app.InfoLog = infoLog
+
+	errorLog = log.New(os.Stdout, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+	app.ErrorLog = errorLog
+
+	session = scs.New()
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = app.InProduction
+
+	app.Session = session
+
+	// // connect to database
+	// log.Println("Connecting to database...")
+	// connectionString := fmt.Sprintf("host=%s port=%s dbname=%s user=%s password=%s sslmode=%s", *dbHost, *dbPort, *dbName, *dbUser, *dbPass, *dbSSL)
+	// db, err := driver.ConnectSQL(connectionString)
+	// if err != nil {
+	// 	log.Fatal("Cannot connect to database! Dying...")
+	// }
+	// log.Println("Connected to database!")
+
+	// tc, err := render.CreateTemplateCache()
+	// if err != nil {
+	// 	log.Fatal("cannot create template cache")
+	// 	return nil, err
+	// }
+
+	app.TemplateCache = tc
+
+	repo := handlers.NewRepo(&app, db)
+	handlers.NewHandlers(repo)
+	render.NewRenderer(&app)
+	helpers.NewHelpers(&app)
+
+	return db, nil
+}
+
+
+
+
+
+
+// package main
+
+// import (
+// 	"net/http"
+
+// 	"github.com/go-chi/chi/v5"
+// 	"github.com/go-chi/chi/v5/middleware"
+// 	"github.com/marceesty/bookings/pkg/config"
+// 	"github.com/marceesty/bookings/pkg/handlers"
+// )
+
+// func routes(app *config.AppConfig) http.Handler {
+
+// 	mux := chi.NewRouter()
+
+// 	mux.Use(middleware.Recoverer)
+// 	mux.Use(NoSurf)
+// 	mux.Use(SessionLoad)
+
+// 	mux.Get("/", handlers.Repo.Home)
+// 	mux.Get("/about", handlers.Repo.About)
+
+// 	fileServer := http.FileServer(http.Dir("./static/"))
+// 	mux.Handle("/static/*", http.StripPrefix("/static", fileServer))
+
+// 	return mux
+// }
